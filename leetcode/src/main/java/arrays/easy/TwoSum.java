da5a939d95c892c0a224b8b5e5b02243a2a72b57
Akhtar <mohamedakhtar2@gmail.com>
2025-03-17 17:39:45 +0530

    "Two Sum sol commited"

--- leetcode/src/main/java/arrays/easy/TwoSum.java
+++ leetcode/src/main/java/arrays/easy/TwoSum.java
@@ -1,5 +1,37 @@
-package arrays.easy;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
 
-public class TwoSum {
+        int[] ans = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
 
+        for(int i =0; i < nums.length; i++){
+            int num = nums[i];
+            int moreNeeded = target -num;
+            if(map.containsKey(moreNeeded)){
+                ans[0] = map.get(moreNeeded);
+                ans[1] = i;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+
+
+
+
+
+
+
+        // int[] ans = new int[2];
+
+        // for(int i=0; i < nums.length; i++){
+        //     for(int j =i+1; j < nums.length; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             ans[0]=i;
+        //             ans[1]=j;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+    }
 }
